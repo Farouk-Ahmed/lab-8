f36f8133342b72740fd3a42c06c5b8b94bb28047
Farouk-Ahmed <140400450+Farouk-Ahmed@users.noreply.github.com>
2023-09-28 15:54:23 +0300

    Add files via upload
    first commit

---
+++ Lab-7/Lab-7/Program.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+//string connectionString = @"Server=\\.\pipe\MSSQL$SQLEXPRESS\sql\query;Database=lab7;Trusted_Connection=True;Encrypt=False;MultipleActiveResultSets=true";
+class Program
+{
+    static string connectionString = @"Data Source=\\.\pipe\MSSQL$SQLEXPRESS\sql\query; Initial Catalog= lab7; Integrated Security =True";
+
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1. Add a customer");
+            Console.WriteLine("2. Delete a customer");
+            Console.WriteLine("3. Update customer ");
+            Console.WriteLine("4. Show all customers");
+            Console.WriteLine("5. Show customer by ID");
+            Console.WriteLine("--------------------------------------------");
+
+            int choice = int.Parse(Console.ReadLine());
+
+
+            switch (choice)
+            {
+                case 1:
+                    AddCustomer();
+                    break;
+                case 2:
+                    DeleteCustomer();
+                    break;
+                case 3:
+                    UpdateCustomer();
+                    break;
+                case 4:
+                    ShowAllCustomers();
+                    break;
+                case 5:
+                    ShowCustomerByID();
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice.");
+                    break;
+            }
+        }
+    }
+
+    static void AddCustomer()
+    {
+        Console.Write("Enter customer name: ");
+        string NAME = Console.ReadLine();
+
+        Console.Write("Enter mobile number: ");
+        string PHONENUMBER = Console.ReadLine();
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            string insertQuery = "INSERT INTO CUSTOMERS (NAME, PHONENUMBER) VALUES (@NAME, @PHONENUMBER)";
+            SqlCommand command = new SqlCommand(insertQuery, connection);
+            command.Parameters.AddWithValue("@NAME", NAME);
+            command.Parameters.AddWithValue("@PHONENUMBER", PHONENUMBER);
+
+            int rowsAffected = command.ExecuteNonQuery();
+
+            if (rowsAffected > 0)
+                Console.WriteLine("Customer added successfully.");
+            else
+                Console.WriteLine("Failed.");
+        }
+    }
+
+    static void DeleteCustomer()
+    {
+        Console.Write("Enter customer ID to delete: ");
+        int ID = int.Parse(Console.ReadLine());
+
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            string deleteQuery = "DELETE FROM CUSTOMERS WHERE ID = @ID";
+            SqlCommand command = new SqlCommand(deleteQuery, connection);
+            command.Parameters.AddWithValue("@ID", ID);
+
+            int rowsAffected = command.ExecuteNonQuery();
+
+            if (rowsAffected > 0)
+                Console.WriteLine("Customer deleted successfully.");
+            else
+                Console.WriteLine("Customer with the specified ID was not found.");
+        }
+    }
+
+    static void UpdateCustomer()
+    {
+        Console.Write("Enter customer ID to update: ");
+        int ID = int.Parse(Console.ReadLine());
+
+
+        Console.Write("Enter new customer name: ");
+        string NAME = Console.ReadLine();
+
+        Console.Write("Enter new mobile number: ");
+        string PHONENUMBER = Console.ReadLine();
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            string updateQuery = "UPDATE CUSTOMERS SET name = @newName, mobile_number = @newMobileNumber WHERE id = @customerId";
+            SqlCommand command = new SqlCommand(updateQuery, connection);
+            command.Parameters.AddWithValue("@NAME", NAME);
+            command.Parameters.AddWithValue("@PHONENUMBER", PHONENUMBER);
+            command.Parameters.AddWithValue("@ID", ID);
+
+            int rowsAffected = command.ExecuteNonQuery();
+
+            if (rowsAffected > 0)
+                Console.WriteLine("Customer information updated successfully.");
+            else
+                Console.WriteLine("Customer with the specified ID was not found.");
+        }
+    }
+
+
+
+    static void ShowCustomerByID()
+    {
+        Console.Write("Enter customer ID to search: ");
+        int ID = int.Parse(Console.ReadLine());
+
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            string selectQuery = "SELECT * FROM CUSTOMERS WHERE ID = @ID";
+            SqlCommand command = new SqlCommand(selectQuery, connection);
+            command.Parameters.AddWithValue("@ID", ID);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            if (reader.Read())
+            {
+                Console.WriteLine();
+                Console.WriteLine($" ID: {reader["ID"]}  Name: {reader["NAME"]}  Mobile Number: {reader["PHONENUMBER"]}");
+
+            }
+            else
+            {
+                Console.WriteLine("Customer with the specified ID was not found.");
+            }
+        }
+    }
+    static void ShowAllCustomers()
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            string selectQuery = "SELECT * FROM CUSTOMERS";
+            SqlCommand command = new SqlCommand(selectQuery, connection);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            Console.WriteLine("CUSTOMERS List:");
+            Console.WriteLine();
+
+            while (reader.Read())
+            {
+                Console.WriteLine($"ID: {reader["ID"]}  Name: {reader["NAME"]} Mobile : {reader["PHONENUMBER"]}");
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+    //static void DisplayCustomers(SqlConnection connection)
+    //{
+    //    string query = "SELECT * FROM CUSTOMERS";
+
+    //    using (SqlCommand command = new SqlCommand(query, connection))
+    //    {
+    //        using (SqlDataReader reader = command.ExecuteReader())
+    //        {
+    //            while (reader.Read())
+    //            {
+    //                Console.WriteLine($"ID: {reader["ID"]}, Name: {reader["NAME"]}, Mobile: {reader["PHONENUMBER"]}");
+    //            }
+    //        }
+    //    }
+    //}
+
+    //static void AddCustomer(SqlConnection connection, string NAME, string PHONENUMBER)
+    //{
+    //    string query = "INSERT INTO CUSTOMERS (NAME, PHONENUMBER) VALUES (NAME, PHONENUMBER)";
+
+    //    using (SqlCommand command = new SqlCommand(query, connection))
+    //    {
+    //        command.Parameters.AddWithValue("NAME", NAME);
+    //        command.Parameters.AddWithValue("PHONENUMBER", PHONENUMBER);
+
+    //        int rowsAffected = command.ExecuteNonQuery();
+
+    //        if (rowsAffected > 0)
+    //        {
+    //            Console.WriteLine("New customer added successfully.");
+    //        }
+    //        else
+    //        {
+    //            Console.WriteLine("Failed to add a new customer.");
+    //        }
+    //    }
+    //}
+
+    //static void DeleteCustomer(SqlConnection connection, int ID)
+    //{
+    //    string query = "DELETE FROM CUSTOMERS WHERE ID = 3";
+
+    //    using (SqlCommand command = new SqlCommand(query, connection))
+    //    {
+    //        command.Parameters.AddWithValue("ID", ID);
+
+    //        int rowsAffected = command.ExecuteNonQuery();
+
+    //        if (rowsAffected > 0)
+    //        {
+    //            Console.WriteLine($"Customer with ID {ID} deleted successfully.");
+    //        }
+    //        else
+    //        {
+    //            Console.WriteLine($"Customer with ID {ID} not found.");
+    //        }
+    //    }
+    //}
+
---
+++ lab-8/lab-8/Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    
+    public static void Main()
+    {
+        Trainee[] Ts = new Trainee[]
+        {
+            new Trainee { name = "Farouk", Age = 25 },
+            new Trainee { name = "Ahmed", Age = 32 },
+            new Trainee { name = "Ola", Age = 28 },
+            new Trainee { name = "Ali", Age = 35 },
+            new Trainee { name = "Kmal", Age = 22 },
+
+            
+        };
+
+        var firstPerson = Ts.First();
+        Console.WriteLine($"First Person: {firstPerson.name}");
+
+        var lastPerson = Ts.Last();
+        Console.WriteLine($"Last Person: {lastPerson.name}");
+
+        var Older = Ts.Where(person => person.Age > 30);
+        Console.WriteLine("Older people:");
+        foreach (var adult in Older)
+        {
+            Console.WriteLine($"{adult.name}, Age {adult.Age}");
+        }
+
+        var Younger = Ts.OrderBy(person => person.Age).Take(3);
+        Console.WriteLine("Younger people :");
+        foreach (var person in Younger)
+        {
+            Console.WriteLine($"{person.name}, Age {person.Age}");
+        }
+
+        var names = Ts.Select(person => person.name);
+        Console.WriteLine("The All Names:");
+        foreach (var name in names)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
+public class Trainee
+{
+    public string name { get; set; }
+    public int Age { get; set; }
+}
+public static class CustomOperators
+{
+    public static TSource First<TSource>(this IEnumerable<TSource> source)
+    {
+        if (source == null)
+        {
+            throw new ArgumentNullException(nameof(source));
+        }
+
+        foreach (var item in source)
+        {
+            return item;
+        }
+
+        throw new InvalidOperationException("No Elements.");
+    }
+
+    public static TSource Last<TSource>(this IEnumerable<TSource> source)
+    {
+        if (source == null)
+        {
+            throw new ArgumentNullException(nameof(source));
+        }
+
+        var lastItem = default(TSource);
+        var found = false;
+
+        foreach (var item in source)
+        {
+            lastItem = item;
+            found = true;
+        }
+
+        if (!found)
+        {
+            throw new InvalidOperationException(" No Elements.");
+        }
+
+        return lastItem;
+    }
+
+    public static IEnumerable<TSource> Where<TSource>(
+        this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+    {
+        if (source == null)
+        {
+            throw new ArgumentNullException(nameof(source));
+        }
+
+        if (predicate == null)
+        {
+            throw new ArgumentNullException(nameof(predicate));
+        }
+
+        foreach (var item in source)
+        {
+            if (predicate(item))
+            {
+                yield return item;
+            }
+        }
+    }
+
+    public static IEnumerable<TSource> Take<TSource>(
+        this IEnumerable<TSource> source, int count)
+    {
+        if (source == null)
+        {
+            throw new ArgumentNullException(nameof(source));
+        }
+
+        if (count < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(count));
+        }
+
+        int co = 0;
+        foreach (var item in source)
+        {
+            if (co >= count)
+            {
+                yield break;
+            }
+
+            yield return item;
+            co++;
+        }
+    }
+
+    public static IEnumerable<TSource> OrderBy<TSource, TKey>(
+        this IEnumerable<TSource> source, Func<TSource, TKey> Selector)
+    {
+        if (source == null)
+        {
+            throw new ArgumentNullException(nameof(source));
+        }
+
+        if (Selector == null)
+        {
+            throw new ArgumentNullException(nameof(Selector));
+        }
+
+        var sortedList = source.ToList();
+        sortedList.Sort((x, y) => Comparer<TKey>.Default.Compare(Selector(x), Selector(y)));
+
+        foreach (var item in sortedList)
+        {
+            yield return item;
+        }
+    }
+
+    public static IEnumerable<TResult> Select<TSource, TResult>(
+        this IEnumerable<TSource> source, Func<TSource, TResult> select)
+    {
+        if (source == null)
+        {
+            throw new ArgumentNullException(nameof(source));
+        }
+
+        if (select == null)
+        {
+            throw new ArgumentNullException(nameof(select));
+        }
+
+        foreach (var item in source)
+        {
+            yield return select(item);
+        }
+    }
+}
+
